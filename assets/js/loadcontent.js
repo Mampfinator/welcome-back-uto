ed3175d7663387287c2a1810b428f4a5e0aec58c
Mampfinator <46071499+Mampfinator@users.noreply.github.com>
2021-01-15 01:38:30 +0100

    Updated layouting & colors

--- assets/js/loadcontent.js
+++ assets/js/loadcontent.js
@@ -1,57 +1,53 @@
-var content = {
-    "Sir Eatsalot": {
-        "type": "text",
-        "original": "I am a test :)", 
-        "translated": "[TBE]"
-    },
-    "Oujimaru": {
-        "type":"text",
-        "original": "I'm Genshin addicted. But I also love Tenshi.", 
-        "translated": "[TBE]"
-    },
-    "Hizu": {
-        "type":"image",
-        "name": "test.png"
-    }, 
-    "Oujimaru2": {
-        "type":"text",
-        "original": "I'm Genshin addicted. But I also love Tenshi.", 
-        "translated": "[TBE]"
-    },    
-    "Oujimaru3": {
-        "type":"text",
-        "original": "I'm Genshin addicted. But I also love Tenshi.", 
-        "translated": "[TBE]"
-    }
-};
-
-
+var content;
 
 $(function() {
+    
+    var currentSmallest;
 
+    $.getJSON("assets/js/messagecontent.json", () => {}).done( (messages)=> {
+        content = messages;
 
-    var entryCounter = 0;
+        for (const [key, value] of Object.entries(messages)) {
+           
+            var toAppend = $("#messages-1");
+            $(".messages").children("div").each((index, value) => {
+                console.log($(value))
+                if (getChildrenHeightSum($(value)) < getChildrenHeightSum(toAppend)) { 
+                    toAppend = $(value)
+                }
+            })
+            console.log("------------")
+            
 
-    for (const [key, value] of Object.entries(content)) {
-        var $curAppend = $(`#messages-${entryCounter%3 + 1}`)
-        console.log($curAppend.attr("id"));
-        if (value.type == "text") {
-            $curAppend.append(`
-                <div class="messages-text-container">
-                    <div>${value.original}</div>
-                    <h3>${key}</h3>
-                </div>
-            `);
-        } else if (value.type == "image") {
-            $curAppend.append(`
-                <a href="images/fulls/${value.name}">
-                    <img src="images/thumbs/${value.name}" alt="" />
-                    <h3>${key}</h3>
-                </a>
-            `)
+            if (value.type == "text") {
+                toAppend.append(`
+                    <div class="messages-text-container" data-language-type="default">
+                        <div>${value.original}</div>
+                        <h3>${key}</h3>
+                    </div>
+                `);
+            } else if (value.type == "image") {
+                toAppend.append(`
+                    <a href="images/fulls/${value.name}">
+                        <img src="images/fulls/${value.name}" alt="" />
+                        <h3>${key}</h3>
+                    </a>
+                `)
+            }
         }
+    })
+})
 
-        entryCounter++;
-    }
+function getChildrenHeightSum(parent) {
+    var sum = 0;
+    parent.children().each( (index, child) => {
+        sum += $(child).height();
+    })
+    return sum;
+}
 
+
+$(".messages-text-container").click(() => {
+    // add logic for translations
+    this.children("div").html()
 })
