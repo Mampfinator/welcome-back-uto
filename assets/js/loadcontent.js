42b7c2aa5277185525879d754c4796ed24c81cfc
Mampfinator <46071499+Mampfinator@users.noreply.github.com>
2021-01-15 14:52:44 +0100

    Fixed layouting, completed translation functionality

--- assets/js/loadcontent.js
+++ assets/js/loadcontent.js
@@ -1,53 +1,79 @@
-var content;
+var messages;
+const messagesFontSize = parseFloat(getComputedStyle($(".messages").get(0)).fontSize);
+
 
 $(function() {
     
     var currentSmallest;
 
-    $.getJSON("assets/js/messagecontent.json", () => {}).done( (messages)=> {
-        content = messages;
+    $.getJSON("assets/js/messagecontent.json", () => {}).done( (data)=> {
+        messages = data;
+
+
+        
 
         for (const [key, value] of Object.entries(messages)) {
            
             var toAppend = $("#messages-1");
-            $(".messages").children("div").each((index, value) => {
-                console.log($(value))
-                if (getChildrenHeightSum($(value)) < getChildrenHeightSum(toAppend)) { 
+            $(".messages").children().each((index, value) => {
+                if (getContentHeightSum($(value)) < getContentHeightSum(toAppend)) { 
                     toAppend = $(value)
                 }
             })
-            console.log("------------")
             
 
             if (value.type == "text") {
                 toAppend.append(`
-                    <div class="messages-text-container" data-language-type="default">
+                    <div class="messages-text-container${"translated" in value? " has-translation" : ""}" data-language="original">
                         <div>${value.original}</div>
                         <h3>${key}</h3>
                     </div>
                 `);
             } else if (value.type == "image") {
                 toAppend.append(`
-                    <a href="images/fulls/${value.name}">
-                        <img src="images/fulls/${value.name}" alt="" />
+                    <a href="images/artworks/${value.name}">
+                        <img src="images/artworks/${value.name}" alt="" />
                         <h3>${key}</h3>
                     </a>
                 `)
             }
         }
+        $(".messages-text-container.has-translation").on("click", function() {
+            $(this).attr("data-language", toggleLanguage($(this).attr("data-language")))
+            var el = this;
+            $($(this).children()[0]).fadeOut(250, function() {
+                var authorName = $($(el).children()[1]).text();
+                $($(el).children()[0]).text(messages[authorName][$(el).attr("data-language")]);
+                $($(el).children()[0]).fadeIn(250);
+            })
+        })
+
+
+        $(".messages").children().each((index, child) => {
+            console.log($(child).height())
+        })
     })
 })
 
-function getChildrenHeightSum(parent) {
+// .height() doesn't work correctly; best thing I could come up with in the meantime as the spacing between elements is always the same
+function getContentHeightSum(parent) {
     var sum = 0;
-    parent.children().each( (index, child) => {
-        sum += $(child).height();
+    parent.children("a, div").each( (index, child) => {
+        sum += child.getBoundingClientRect().height + 2*messagesFontSize;
+        if ($($(child).find("img")[0]).is("img")) {
+            var imgChild = $($(child).find("img")[0]);
+            sum += imgChild.prop("naturalHeight")*((screen.width/3)/imgChild.prop("naturalWidth"))
+        }
     })
     return sum;
 }
 
+// lazy way of doing it but hey, if it works, it works
+function toggleLanguage(curLang) {
+    if (curLang == "original") {
+        return "translated"
+    } else {
+        return "original"
+    }
+}
 
-$(".messages-text-container").click(() => {
-    // add logic for translations
-    this.children("div").html()
-})
