2f72a5c872e9973610740f4b4885212a63aa086d
Mampfinator <46071499+Mampfinator@users.noreply.github.com>
2021-01-17 16:50:04 +0100

    (hopefully) fixed mobile background issues

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,15 @@
 	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
 
+
+function isMobile() {
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+		return true;
+	}
+	return false;
+}
+
+
 $(function() {
 
 	// Vars.
@@ -21,17 +30,18 @@ $(function() {
 		});
 
 
-
-		$( window ).scroll( function(){
-			var ypos = $( window ).scrollTop(); //pixels the site is scrolled down
-			var visible = $( window ).height(); //visible pixels
-			const img_height = 1950; //replace with height of your image
-			var max_scroll = img_height - visible; //number of pixels of the image not visible at bottom
-		  //change position of background-image as long as there is something not visible at the bottom  
-		  if ( max_scroll > ypos ) {
-			   $('body').css('background-position', "center -" + ypos + "px");
-			} else {
-			  $('body').css('background-position', "center -" + max_scroll + "px");
-			}
-		  });
+		if (!isMobile()) {
+			$( window ).scroll( function(){
+				var ypos = $( window ).scrollTop(); //pixels the site is scrolled down
+				var visible = $( window ).height(); //visible pixels
+				const img_height = 1950; //replace with height of your image
+				var max_scroll = img_height - visible; //number of pixels of the image not visible at bottom
+			//change position of background-image as long as there is something not visible at the bottom  
+			if ( max_scroll > ypos ) {
+				$('body').css('background-position', "center -" + ypos + "px");
+				} else {
+				$('body').css('background-position', "center -" + max_scroll + "px");
+				}
+			});
+		}
 });
